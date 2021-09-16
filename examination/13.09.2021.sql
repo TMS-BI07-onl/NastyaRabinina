53c071fbc166764c5d37e95758889033ec3f3809
Stacy-Bini <a.s.rabinina@gmail.com>
2021-09-16 03:14:45 +0300

    exam

--- examination/13.09.2021.sql
+++ examination/13.09.2021.sql
@@ -6,7 +6,8 @@ SELECT *
 from  Person.Person
 
 SELECT Person.PersonPhone.PhoneNumber,Person.Person.FirstName,Person.Person.LastName
-FROM [Person].[Person], [Person].[PersonPhone]
+FROM [Person].[Person] p
+JOIN [Person].[PersonPhone] pp ON p.BusinessEntityID= pp.BusinessEntityID
 WHERE  Person.PersonPhone.PhoneNumber LIKE '4_5%'
 
 2) ќтнести каждого человека из [HumanResources].[Employee] в свою возрастную категорию:
@@ -15,6 +16,16 @@ Adults: 21-59
 Elderly: 60-75
 Senile: 76-90
 
+SELECT FirstName, LastName,
+    CASE WHEN DATEDIFF(year, BirthDate, GETDATE()) BETWEEN 17 AND 20 THEN 'Adolescence'
+	     WHEN DATEDIFF(year, BirthDate, GETDATE()) <=59 THEN 'Adults'
+		 WHEN DATEDIFF(year, BirthDate, GETDATE()) <=75 THEN 'Elderly'
+		 WHEN DATEDIFF(year, BirthDate, GETDATE()) <=90 THEN 'Sentile'
+    END
+	AS Category
+FROM [HumanResources].[Employee] h
+JOIN [Person].[Person] pp
+ON h.BusinessEntityID=pp.BusinessEntityID;
 
 
 3) ¬ывести самый дорогой продукт дл€ каждого цвета из [Production].[Product]
@@ -37,8 +48,6 @@ CREATE TABLE Pupil (
 	Class INT NOT NULL,
 	TeacherID INT NOT NULL,
 	SchoolID INT NOT NULL
-	,CONSTRAINT FK_Pupil_To_Teacher FOREIGN KEY (TeacherId)  REFERENCES Teacher (Id)
-	,CONSTRAINT FK_Pupil_To_School FOREIGN KEY (SchoolId)  REFERENCES School (Id)
 )
 
 CREATE TABLE Teacher (
@@ -51,4 +60,37 @@ CREATE TABLE Teacher (
 	SchoolID INT NOT NULL
 )
 
+CREATE TABLE Class (
+    ID INT CONSTRAINT PK_Class_Id PRIMARY KEY IDENTITY NOT NULL,
+	Number INT NOT NULL,
+	Letter NVARCHAR(2) NOT NULL,
+	Direction NVARCHAR(20) NOT NULL UNIQUE, 
+)
 
+CREATE TABLE Subject (
+    ID INT CONSTRAINT PK_Subject_Id PRIMARY KEY IDENTITY NOT NULL,
+	Name NVARCHAR(50) NOT NULL UNIQUE
+)
+
+CREATE TABLE Cabinet (
+    ID INT CONSTRAINT PK_Cabinet_Id PRIMARY KEY IDENTITY NOT NULL,
+	Number INT NOT NULL,
+	Name NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE School (
+    ID INT CONSTRAINT PK_School_Id PRIMARY KEY IDENTITY NOT NULL,
+	Name NVARCHAR(50) NOT NULL,
+	Adress NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Parents (
+    ID INT CONSTRAINT PK_Parent_Id PRIMARY KEY IDENTITY NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	PassportNumber NVARCHAR(20) NOT NULL UNIQUE, 
+	Gender NVARCHAR(1) NOT NULL,
+	Adress NVARCHAR(50) NOT NULL
+	TelNum INT NOT NULL,
+	SchoolID INT NOT NULL
+)
